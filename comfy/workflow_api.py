0a2e8b2a9a837e28109556afa7c2f8ce32da23b6
epochlab <JRoberts86@gmail.com>
2024-08-22 22:49:23 +0100

    Add flags

--- comfy/workflow_api.py
+++ comfy/workflow_api.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import warnings
+warnings.filterwarnings('ignore')
+
 import random
 from dataclasses import dataclass
 import torch
@@ -18,7 +21,7 @@ class HyperConfig:
     upscale = "RealESRGAN_x2.pth"
 
     prompt = "a photograph of a red starfish, ((high resolution, high-resolution, cinematic, technicolor, film grain, analog, 70mm, 8K, IMAX, Nat Geo, DSLR))"
-    negative = "worst quality, low quality, low-res, low details, cropped, blurred, defocus, bokeh, oversaturated, undersaturated, overexposed, underexposed, letterbox, aspect ratio, formatted, jpeg artefacts, draft, glitch, error, deformed, distorted, disfigured, duplicated, bad proportions"
+    negative = "worst quality, low quality, low-res, low details, cropped, blurred, defocus, bokeh, oversaturated, undersaturated, overexposed, underexposed, letterbox, aspect ratio, formatted, jpeg artefacts, draft, glitch, error, deformed, distorted, disfigured, duplicated, bad proportions, teeth"
 
     VERSION = "v001"
     SOURCE = f"/mnt/vanguard/STAGE/render/{VERSION}/stage_{VERSION}_"
@@ -26,7 +29,7 @@ class HyperConfig:
     depth = str(SOURCE + "depth.png")
     curvature = str(SOURCE + "curvature.png")
 
-    depth_stength = 2
+    depth_stength = 1.0
     canny_stength = 0.5
 
     sampler = "dpmpp_2m"
@@ -36,6 +39,8 @@ class HyperConfig:
     denoise = 0.75
     cfg_scale = 8.0
 
+    enable_img2img = True
+    enable_controlnet = True
     enable_upscale = True
 
 def main():
@@ -55,16 +60,21 @@ def main():
         clipencode_prompt = clipencode.encode(text=config.prompt, clip=ckpt[1])
         clipencode_negative = clipencode.encode(text=config.negative, clip=ckpt[1])
 
-        controlnet = ControlNetApply()
-        controlnetloader = ControlNetLoader()
-        controlnetloader_depth = controlnetloader.load_controlnet(control_net_name=config.cnet_depth)
-        controlnetloader_canny = controlnetloader.load_controlnet(control_net_name=config.cnet_canny)
+        if config.enable_controlnet:
+            controlnet = ControlNetApply()
+            controlnetloader = ControlNetLoader()
+            controlnetloader_depth = controlnetloader.load_controlnet(control_net_name=config.cnet_depth)
+            controlnetloader_canny = controlnetloader.load_controlnet(control_net_name=config.cnet_canny)
 
-        clip_depth = controlnet.apply_controlnet(strength=config.depth_stength, conditioning=clipencode_prompt[0], control_net=controlnetloader_depth[0], image=z[0])
-        clip_canny = controlnet.apply_controlnet(strength=config.canny_stength, conditioning=clip_depth[0], control_net=controlnetloader_canny[0], image=curv[0])
+            clip_depth = controlnet.apply_controlnet(strength=config.depth_stength, conditioning=clipencode_prompt[0], control_net=controlnetloader_depth[0], image=z[0])
+            clip_canny = controlnet.apply_controlnet(strength=config.canny_stength, conditioning=clip_depth[0], control_net=controlnetloader_canny[0], image=curv[0])
+            clip = clip_canny[0]
+        else:
+            clip = clipencode_prompt[0]
 
-        if config.enable_upscale == True:
+        if config.enable_upscale:
             upscaler = NODE_CLASS_MAPPINGS["UpscaleModelLoader"]().load_model(model_name=config.upscale)
+            print("Upsampling: Enabled")
 
         seed = 123 #random.randint(1, 2**64)
         print(f"Seed: {seed}")
@@ -78,14 +88,14 @@ def main():
                 scheduler=config.scheduler,
                 denoise=config.denoise,
                 model=ckpt[0],
-                positive=clip_canny[0],
+                positive=clip,
                 negative=clipencode_negative[0],
                 latent_image=enc[0],
             )
 
             out = VAEDecode().decode(samples=latents[0], vae=ckpt[2])
 
-            if config.enable_upscale == True:
+            if config.enable_upscale:
                 out = NODE_CLASS_MAPPINGS["ImageUpscaleWithModel"]().upscale(upscale_model=upscaler[0], image=out[0])
 
             SaveImage().save_images(filename_prefix="STAGE_diffusion", images=out[0])
